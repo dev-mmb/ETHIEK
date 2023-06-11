1ec8bb5eb14247e8855303073e2b1b5ea434c326
meesmb <meesbarnhoorn@gmail.com>
2023-06-11 11:29:38 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {lastValueFrom} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'ethiek';
+export class AppComponent implements OnInit{
+  url = "https://thispersondoesnotexist.com/";
+  currentUrl: string = "";
+  shouldLoad = true;
+
+  showChallenges = true;
+  challenges = [
+    {text: 'Challenge: Krant van vandaag', checked: true},
+    {text: 'Challenge: Lepel op hoofd', checked: false},
+    {text: 'Challenge: Landmark', checked: false},
+    {text: 'Challenge: Handstand', checked: false},
+  ]
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  onClick() {
+    this.currentUrl = this.url + "?" + new Date().getTime();
+  }
+
+  ngOnInit(): void {
+    this.onClick();
+  }
+
+  setChallenges(value: boolean) {
+    this.showChallenges = value;
+  }
+
+  onChallengeClick(challenge: any) {
+    challenge.checked = true;
+  }
 }
